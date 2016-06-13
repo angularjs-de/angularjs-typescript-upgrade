9ca58c01276c2f581fa8631bff3a3d0067047556
Bengt Weiße <bengtler@gmail.com>
2016-06-13 16:11:07 +0200

    upgrade $location service and downgrade navigation component

--- app/app.ts
+++ app/app.ts
@@ -5,7 +5,7 @@ import {upgradeAdapter} from './upgrade_adapter';
 
 import routes from './routes'
 
-import navigation from './components/navigation/navigation'
+import {NavigationComponent} from './components/navigation/navigation'
 import booksIndex from './components/books-index/books-index'
 import booksShow from './components/books-show/books-show'
 
@@ -13,9 +13,9 @@ import booksShow from './components/books-show/books-show'
 angular.module('myApp', [
   'ngRoute',
   routes,
-  navigation,
   booksIndex,
   booksShow
 ])
+.directive('navigation', upgradeAdapter.downgradeNg2Component(NavigationComponent);
 
 upgradeAdapter.bootstrap(document.body, ['myApp']);
--- app/components/navigation/navigation.ts
+++ app/components/navigation/navigation.ts
@@ -1,21 +1,23 @@
-import * as angular from 'angular'
+import {Component, Inject} from '@angular/core';
+import {upgradeAdapter} from '../../upgrade_adapter';
 
-const moduleName = 'myApp.navigation'
-export default moduleName
+upgradeAdapter.upgradeNg1Provider('$location');
 
-class Navigation {
-  constructor (
-    private $location: angular.ILocationService
-  ) {}
-  
+// angular.ILocationService
+
+@Component({
+  selector: 'navigation',
+  templateUrl: 'app/components/navigation/navigation.html'
+})
+export class NavigationComponent {
+  private $location;
+
+  constructor(@Inject('$location') $location) {
+    this.$location = $location;
+  }
+
   private isActive (route: string): boolean {
     const routeExp = new RegExp(route)
     return routeExp.test(this.$location.path())
   }
 }
-
-angular.module(moduleName, [])
-  .component('navigation', {
-    templateUrl: 'app/components/navigation/navigation.html',
-    controller: Navigation
-  })
